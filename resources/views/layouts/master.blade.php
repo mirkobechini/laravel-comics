507b60bf5d6157af9d8d20ca51600959845fabfa
mirkobechini <mirkobechini@gmail.com>
2025-12-29 20:38:18 +0100

    Add: master layout file

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,10 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Titolo</title>
+    <title>@yield("title")</title>
     @vite(["resources/sass/app.scss", "resources/js/app.js"])
 </head>
 <body>
     @include("partials.header")
+    <div class="bg-dark">
+        sdas
+    </div>
 </body>
 </html>
