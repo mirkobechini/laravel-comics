ebb1951526598ae668967920a75dc562afc00bc0
mirkobechini <mirkobechini@gmail.com>
2025-12-30 14:46:42 +0100

    Add: implement comics view, update header links, and enhance active link styling

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -7,14 +7,14 @@ $menu = require base_path("./database/data/navbar.php");
 <header>
     <nav class="navbar navbar-expand bg-body-light">
         <div class="container d-flex justify-content-between my-2">
-            <a class="navbar-brand" href="#">
+            <a class="navbar-brand" href="{{route('welcome')}}">
                 <img width="70" src="{{ Vite::asset('resources/img/dc-logo.png')}}" alt="">
             </a>
             <div class="navbar" id="navbarNav">
                 <ul class="navbar-nav ">
                     @foreach ($menu as $item)
                     <li class="nav-item px-1 text-uppercase">
-                        <a class="nav-link roboto-condensed-400 text-dark" href="#">{{$item}}</a>
+                        <a class='nav-link roboto-condensed-400 text-dark {{ request()->is($item) ? "active" : "" }}' href="{{$item}}">{{$item}}</a>
                     </li>
                     @endforeach
                 </ul>
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name("welcome");
+
+Route::get('/comics', function () {
+    return view('comics');
+})->name("comics");
